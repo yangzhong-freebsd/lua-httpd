6ee2dc99973ad60090d5290fdc5d3fc9028da525
Ryan Moeller <ryan@freqlabs.com>
2020-05-28 07:10:14 +0000

    Add disk selection

---
+++ disk.lua
@@ -0,0 +1,61 @@
+-- vim: set et:
+--
+-- Copyright (c) 2020 Ryan Moeller <ryan@freqlabs.com>
+--
+-- Permission to use, copy, modify, and distribute this software for any
+-- purpose with or without fee is hereby granted, provided that the above
+-- copyright notice and this permission notice appear in all copies.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+-- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+-- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+-- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+-- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+--
+
+local disk = disk or {}
+
+local function kern_disks()
+    local disksfile = "/tmp/disks"
+    assert(os.execute("sysctl -n kern.disks >" .. disksfile))
+    local f = assert(io.open(disksfile, "r"))
+    local text = f:read("*a")
+    f:close()
+    return text
+end
+
+local function diskinfo(dev)
+    local descfile = "/tmp/desc"
+    assert(os.execute("sudo -u root diskinfo -v "..dev.." >" .. descfile))
+    local f = assert(io.open(descfile, "r"))
+    local text = f:read("*a")
+    f:close()
+    return text
+end
+
+function disk.info()
+    local disks = {}
+    local text = kern_disks()
+    for dev in text:gmatch("([^ \n]+)") do
+        local disk = {}
+        local text = diskinfo(dev)
+        for line in text:gmatch("([^\n]+)") do
+            if line:find("#") ~= nil then
+                local value, field = line:match("^\t([^\t]+)\t+# (.*)$")
+                local f, v = field:match("(.*) %((.*)%)")
+                if f ~= nil then
+                        disk[f] = value
+                        disk[f .. " human"] = v
+                else
+                        disk[field] = value
+                end
+            end
+        end
+        disks[dev] = disk
+    end
+    return disks
+end
+
+return disk
