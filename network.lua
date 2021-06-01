1ef673f74970dbd0661b827565eff7d991bb1a62
Yang <yzhong@freebsdfoundation.org>
2021-06-01 13:22:58 -0700

    add really basic wireless connection

--- network.lua
+++ network.lua
@@ -55,7 +55,7 @@ end
 function network.scanWireless() -- (interface)
 	local networks = {}
 	local one, two = os.execute("wpa_cli scan >/dev/null 2>&1")
-	os.execute("sleep 1")
+	os.execute("sleep 5")
 	for line in io.popen("wpa_cli scan_result"):lines() do
 		if line:match("(.*)\t(.*)\t(.*)\t(.*)\t(.*)") then
 			local bssid, frequency, signal_level, flags, ssid = line:match("(.*)\t(.*)\t(.*)\t(.*)\t(.*)")
@@ -72,6 +72,32 @@ function network.scanWireless() -- (interface)
 	end
 
 	return networks
+end
+
+function network.connectWireless(network, password)
+	local wpa_sup_path = "/etc/wpa_supplicant.conf"
+	local wpa_sup = io.open(wpa_sup_path, "w+")
+
+	wpa_sup:write("ctrl_interface=/var/run/wpa_supplicant\n")
+	wpa_sup:write("eapol_version=2\n")
+	wpa_sup:write("ap_scan=1\n")
+	wpa_sup:write("fast_reauth=1\n")
+
+	wpa_sup:write("network={\n")
+	wpa_sup:write("\tpriority=0\n")
+	wpa_sup:write("\tkey_mgmt=NONE\n")
+	wpa_sup:write("}\n")
+
+	--TODO support things other than PSK
+	wpa_sup:write("network={\n")
+	wpa_sup:write("\tssid=\""..network.."\"\n")
+	wpa_sup:write("\tscan_ssid=0\n")
+	wpa_sup:write("\tpsk=\""..password.."\"\n")
+	wpa_sup:write("\tpriority=5\n")
+	wpa_sup:write("}\n")
+	wpa_sup:close()
+
+	os.execute("wpa_cli reconfigure")
 end
 
 checkIPv4()
