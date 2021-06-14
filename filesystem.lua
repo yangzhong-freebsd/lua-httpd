529323e09477fbefd955923737d5b6f96287c56b
Yang <yzhong@freebsdfoundation.org>
2021-06-14 12:29:30 -0700

    add simple zfs disk picker

--- filesystem.lua
+++ filesystem.lua
@@ -61,4 +61,31 @@ filesystem.formats = {
     },
 }
 
+filesystem.zfs_formats = {
+    {
+        title = "Stripe",
+        value = "ZFS:STRIPE",
+        default = true,
+        min_disks = 1,
+    },
+    {
+        title = "Mirror",
+        value = "ZFS:MIRROR",
+        default = false,
+        min_disks = 2,
+    },
+    {
+        title = "RAID-Z1",
+        value = "ZFS:RAIDZ1",
+        default = false,
+        min_disks = 3,
+    },
+    {
+        title = "RAID-Z2",
+        value = "ZFS:RAIDZ2",
+        default = false,
+        min_disks = 4,
+    },
+}
+
 return filesystem
